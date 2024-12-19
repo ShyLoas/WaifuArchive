c053de988fb6ac83045f756e289c500b4743e043
ShyLoas <154396049+ShyLoas@users.noreply.github.com>
2024-12-19 18:24:27 +0700

    PointLine.java

--- src/Controller/PointLine.java
+++ src/Controller/PointLine.java
@@ -6,7 +6,8 @@ import java.awt.*;
 class PointLine {
     public Point p1;
     public Point p2;
-    
+
+    //hàm lấy đường thẳng của hai điểm
     public PointLine(Point p1, Point p2){
         super();
         this.p1 = p1;
