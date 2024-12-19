a572649d619f1a39d5e7afb015131e752095d70f
ShyLoas <154396049+ShyLoas@users.noreply.github.com>
2024-12-19 18:35:13 +0700

    Main.java

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -11,8 +11,8 @@ public class Main {
         new Thread(frame).start();
     }
 
+    //tạo hàm quản lý thời gian trò chơi
     class TimeCount extends Thread {
-
         @Override
         public void run() {
             while (true) {
@@ -25,10 +25,11 @@ public class Main {
                     if (frame.isResume()) {
                         frame.time--;
                     }
-                } else {
+                } else { //thời gian liên tục giảm khi không dừng
                     frame.time--;
                 }
                 if (frame.time == 0) {
+                    //nếu người chơi hết thời gian, hiển thị thông báo
                     if (frame.showDialogNewGame("Full Time\nPlay again?", "Lose", 1) == true) {
                     }
                 }
